0393ec5cd7d931f716a007cf497e266067156da2
leikang123 <42248685+leikang123@users.noreply.github.com>
2021-06-30 15:32:17 +0800

    Update Popedom.java

--- OA/src/main/java/org/fkit/oa/identity/domain/Popedom.java
+++ OA/src/main/java/org/fkit/oa/identity/domain/Popedom.java
@@ -7,9 +7,7 @@ import javax.persistence.*;
 
 
 /**
- * @Author xlei @tel 13360026135 @qq 251425887
- * @Date 2015年9月6日下午1:12:42
- * @Email dlei0009@163.com
+ * 
  * @Version 1.0
  * @From http://www.fkit.org
  *
