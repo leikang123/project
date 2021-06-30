172298fffa7bfd8f677184cba49cee4ca545fa7c
leikang123 <42248685+leikang123@users.noreply.github.com>
2021-06-30 15:28:04 +0800

    Update LoginController.java

--- OA/src/main/java/org/fkit/oa/identity/controller/LoginController.java
+++ OA/src/main/java/org/fkit/oa/identity/controller/LoginController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+// 登陆控制类
 @Controller
 public class LoginController {
 	
