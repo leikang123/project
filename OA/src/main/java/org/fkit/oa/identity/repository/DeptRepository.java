b34038034a99a9d4c7b3a9587a94296ae1eceabf
leikang123 <42248685+leikang123@users.noreply.github.com>
2021-06-30 15:39:00 +0800

    部门存储接口功能

--- OA/src/main/java/org/fkit/oa/identity/repository/DeptRepository.java
+++ OA/src/main/java/org/fkit/oa/identity/repository/DeptRepository.java
@@ -8,13 +8,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 /**
- * @author xlei
- * @Email dlei0006@163.com
- * @QQ 251425887
- * @Tel 18665616520
+ *
  * @Date 2017年1月14日上午9:44:47
  * @From http://www.fkjava.org 
- *
+ *部门存储接口
  */
 public interface DeptRepository extends JpaRepository<Dept, Long>{
 	
